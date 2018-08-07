8f04621fdc79d1305a217d1737fb271a8e671673
Sergejs Demarchuks <sergejsd336@gmail.com>
2018-08-07 08:52:34 +0300

    Initial Commit

---
+++ src/main/java/com/sergejs/blog/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.sergejs.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/")
+    public String index(){
+        return "hello";
+    }
+
+
+}
